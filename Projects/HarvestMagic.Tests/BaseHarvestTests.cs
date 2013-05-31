7a62a53c985efbffb1b508a758e7f1b760561cd4
Tim Murphy <tim@26tp.com>
2013-06-01 06:01:42 +1000

    Completed outstanding tests.

--- Projects/HarvestMagic.Tests/BaseHarvestTests.cs
+++ Projects/HarvestMagic.Tests/BaseHarvestTests.cs
@@ -3,11 +3,12 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.Reflection;
+using HarvestMagic.Core;
 
 namespace HarvestMagic.Tests
 {
     [TestClass]
-    public abstract class BaseHarvestTests<T>
+    public abstract class BaseHarvestTests<T> where T : BaseHarvestApi
     {
         [TestMethod]
         public void Constructor_ShouldThrowArgumentNullExceptionWhenAccountIsNull()
@@ -52,25 +53,57 @@ namespace HarvestMagic.Tests
         [TestMethod]
         public void Account_GetSet()
         {
-            Assert.Inconclusive("todo");
+            // Given
+            var harvest = (T)Activator.CreateInstance(typeof(T), new object[] { new HarvestAccount() { Password = "fake", Uri = "http://fake.com", UserName = "fake" } });
+            var account = new HarvestAccount() { Password = "fake", Uri = "http://fake.com", UserName = "fake" };
+
+            // When
+            harvest.Account = account;
+
+            // Then
+            harvest.Account.Should().BeSameAs(account);
         }
 
         [TestMethod]
         public void Account_ShouldThrowArgumentNullExceptionWhenValueIsNull()
         {
-            Assert.Inconclusive("todo");
+            // Given
+            var harvest = (T)Activator.CreateInstance(typeof(T), new object[] { new HarvestAccount() { Password = "fake", Uri = "http://fake.com", UserName = "fake" } });
+
+            // When
+            Action action = () => harvest.Account = null;
+
+            // Then
+            action.ShouldThrow<ArgumentNullException>()
+                .Subject.Message.EndsWith("Parameter name: value");
         }
 
         [TestMethod]
         public void Account_ShouldThrowArgumentNullExceptionWhenValueIsNotValid()
         {
-            Assert.Inconclusive("todo");
+            // Given
+            var harvest = (T)Activator.CreateInstance(typeof(T), new object[] { new HarvestAccount() { Password = "fake", Uri = "http://fake.com", UserName = "fake" } });
+
+            // When
+            Action action = () => harvest.Account = new HarvestAccount();
+
+            // Then
+            action.ShouldThrow<ValidationException>();
         }
 
         [TestMethod]
         public void Account_SetShouldFreezeValue()
         {
-            Assert.Inconclusive("todo");
+            // Given
+            var accountForConstructor = new HarvestAccount() { Password = "fake", Uri = "http://fake.com", UserName = "fake" };
+            var accountForProperty = new HarvestAccount() { Password = "fake", Uri = "http://fake.com", UserName = "fake" };
+            var harvest = (T)Activator.CreateInstance(typeof(T), new object[] { accountForConstructor });
+
+            // When
+            harvest.Account = accountForProperty;
+
+            // Then
+            ((bool)accountForProperty.GetType().GetField("isFrozen", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(accountForProperty)).Should().BeTrue();
         }
     }
 }
